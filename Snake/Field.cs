a1718143a57195fd5b9ef816391593abbe78a513
Sergey Makletsov <smaklets@kpfu.ru>
2022-06-13 10:55:42 +0300

    Добавьте файлы проекта.

---
+++ Snake/Field.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace Snake
+{
+    public class Field
+    {
+        private Random _rand;
+        private int _rows;
+        private int _cols;
+        private Snake _snake;
+        private Food _food;
+
+        public List<Point> SnakePosition => _snake.Coords;
+        public Point FoodPosition => _food.Position;
+
+        public Direction SnakeDirection
+        {
+            get => _snake.CurrentDirection;
+            set
+            {
+                _snake.Turn(value);
+            }
+        }
+        public int Rows
+        {
+            get => _rows;
+            set
+            {
+                if (value < 10) value = 10;
+                if (value > 100) value = 100;
+                _rows = value;
+            }
+        }
+
+        public int Columns
+        {
+            get => _cols;
+            set
+            {
+                if (value < 10) value = 10;
+                if (value > 100) value = 100;
+                _cols = value;
+            }
+        }
+
+        public Field() : this(30, 30)
+        {
+        }
+
+        public Field(int rows, int cols)
+        {
+            Rows = rows;
+            Columns = cols;
+            _rand = new Random((int)DateTime.Now.Ticks);
+            _snake = new Snake(new Point(_rand.Next(3, Rows - 3), _rand.Next(3, Columns - 3)));
+            NewFood();
+        }
+
+        private void NewFood()
+        {
+            do
+            {
+                _food = new Food(_rand.Next(3, Rows - 3), _rand.Next(3, Columns - 3));
+            } while (_snake.Contains(_food.Position));
+        }
+        public void NextStep()
+        {
+            _snake.Crawl();
+            if (_snake.Ate(_food))
+            {
+                _snake.Grow();
+                NewFood();
+            }
+        }
+    }
+}
---
+++ Snake/Form1.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+namespace Snake
+{
+    public partial class Form1 : Form
+    {
+        private Field _field;
+        private FieldController _fieldController;
+        private FieldView _fieldView;
+        public Form1()
+        {
+            InitializeComponent();
+            _field = new Field();
+            _fieldController = new FieldController(_field);
+            _fieldView = new FieldView(_field);
+        }
+
+        private void panel1_DoubleClick(object sender, EventArgs e)
+        {
+            _fieldController.Start();
+            _fieldView.StartOn(panel1.CreateGraphics());
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            e.Handled = true;
+            var d = new Dictionary<Keys, Direction>()
+            {
+                {Keys.Left, Direction.ToLeft},
+                {Keys.Right, Direction.ToRight},
+                {Keys.Up, Direction.ToTop},
+                {Keys.Down, Direction.ToBottom}
+            };
+            try
+            {
+                _field.SnakeDirection = d[e.KeyCode];
+            } catch {}
+        }
+    }
+}
